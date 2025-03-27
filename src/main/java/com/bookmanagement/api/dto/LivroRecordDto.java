dbb4b8479f9b13b1245d9dfa7bebc42faf9d3a8f
Gildemar <gildemardiniz@gmail.com>
2025-03-27 20:38:57 -0300

    Implementada validação @NotBlank e @NotNull, com mensagens de erro personalizadas.

--- src/main/java/com/bookmanagement/api/dto/LivroRecordDto.java
+++ src/main/java/com/bookmanagement/api/dto/LivroRecordDto.java
@@ -4,12 +4,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record LivroRecordDto(
-        @NotBlank
+        @NotBlank(message = "Erro: O título não pode estar vazio. Por favor, insira um valor válido.")
         String titulo,
-        @NotBlank
+        @NotBlank(message = "Erro: O autor não pode estar vazio. Por favor, insira um valor autor.")
         String autor ,
-        @NotNull
+        @NotNull(message = "Erro: O ano de publicação não pode estar vazio. Por favor, insira um valor válido.")
         Integer anoPublicacao
-
 ) {
 }
