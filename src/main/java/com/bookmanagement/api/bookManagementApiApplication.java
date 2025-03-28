5595d53cc5a91952e2143d794a9f818eeba3fa3b
Gildemar <gildemardiniz@gmail.com>
2025-03-28 14:27:51 -0300

    Refatorado o nome Application e ApplicationTest

--- src/main/java/com/bookmanagement/api/bookManagementApiApplication.java
+++ src/main/java/com/bookmanagement/api/bookManagementApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CApplication {
+public class bookManagementApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CApplication.class, args);
+		SpringApplication.run(bookManagementApiApplication.class, args);
 	}
 
 }
