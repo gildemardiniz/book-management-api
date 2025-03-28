73bf45dc77d57ea197065b0eb01d6d0adb120f27
Gildemar <gildemardiniz@gmail.com>
2025-03-28 14:21:11 -0300

    Adiciona testes unitários para métodos do LivroService
    - Implementa testes para os métodos save, findAll, findById, update e delete do LivroService.
    - Adiciona validação de exceção ObjectNotFoundException nos casos de update e delete quando o livro não existe.
    - Adiciona verificação de sucesso ao salvar e deletar um livro, com assertivas de objetos retornados e chamadas ao repositório.
    - Garantia de comportamento esperado nas operações de criação, busca, atualização e exclusão de livros.

---
+++ src/test/java/com/bookmanagement/api/controller/LivroControllerTest.java
@@ -0,0 +1,106 @@
+package com.bookmanagement.api.controller;
+
+import com.bookmanagement.api.dto.LivroRecordDto;
+import com.bookmanagement.api.model.LivroModel;
+import com.bookmanagement.api.service.LivroService;
+import org.hibernate.ObjectNotFoundException;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+@SpringBootTest
+class LivroControllerTest {
+
+    @InjectMocks
+    LivroController livroController;
+
+    @Mock
+    LivroService livroService;
+
+    LivroModel livroTeste = new LivroModel(
+            1L,
+            "As crônicas de Nárnia - O leão, a feiticeira e o guarda-roupa",
+            "C. S. Lewis",
+            1950
+    );
+
+    LivroRecordDto livroRecordDtoTest = new LivroRecordDto(
+            "As crônicas de Nárnia - O leão, a feiticeira e o guarda-roupa",
+            "C. S. Lewis",
+            1950
+    );
+
+    @Test
+    void aoSalvarLivroRetornarStatusCriadoEObjeto() {
+        Mockito.when(livroService.save(Mockito.any(LivroModel.class))).thenReturn(livroTeste);
+        ResponseEntity<Object> retorno = livroController.save(livroRecordDtoTest);
+        assertNotNull(retorno);
+        assertEquals(HttpStatus.CREATED, retorno.getStatusCode());
+        assertEquals(livroTeste, retorno.getBody());
+    }
+
+    @Test
+    void aoSalvarLivroDeveRetornarErroInterno() {
+        Mockito.when(livroService.save(Mockito.any(LivroModel.class))).thenThrow(new RuntimeException());
+        ResponseEntity<Object> retorno = livroController.save(livroRecordDtoTest);
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR,retorno.getStatusCode());
+    }
+
+    @Test
+    void aoBuscarTodosOsLivroDeveRetornarErroInterno() {
+        Mockito.when(livroService.findAll()).thenThrow(new RuntimeException());
+        ResponseEntity<Object> retorno = livroController.findAll();
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR,retorno.getStatusCode());
+    }
+    @Test
+    void aoBuscarTodosLivrosDeveRetornarStatusOk() {
+      Mockito.when(livroService.findAll()).thenReturn(Collections.singletonList(livroTeste));
+      ResponseEntity<Object> retorno = livroController.findAll();
+      assertEquals(HttpStatus.OK,retorno.getStatusCode());
+    }
+
+    @Test
+    void atualizarLivroDeveRetornarBadRequest() {
+        Mockito.when(livroService.update(Mockito.anyLong(),Mockito.any(LivroRecordDto.class))).thenThrow(new ObjectNotFoundException(1L,"Erro: O ID informado não foi encontrado. Por favor, verifique e tente novamente."));
+        ResponseEntity<Object> retorno = livroController.update(1L,livroRecordDtoTest);
+        assertEquals(HttpStatus.NOT_FOUND,retorno.getStatusCode());
+    }
+
+    @Test
+    void aoAtualizarLivroDeveRetornarInternalServerError() {
+        Mockito.when(livroService.update(Mockito.anyLong(),Mockito.any(LivroRecordDto.class))).thenThrow(new RuntimeException());
+        ResponseEntity<Object> retorno = livroController.update(1L,livroRecordDtoTest);
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR,retorno.getStatusCode());
+    }
+
+    @Test
+    void aoDeletarLivroDeveRetornarInternalServerError() {
+        Mockito.doThrow(new RuntimeException()).when(livroService).deleteById(Mockito.anyLong());
+        ResponseEntity<Object> retorno = livroController.delete(1L);
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR,retorno.getStatusCode());
+    }
+
+    @Test
+    void aoDeletarLivroDeveRetornarBadRequest() {
+        Mockito.doThrow(new ObjectNotFoundException(1L,"Erro: O ID informado não foi encontrado. Por favor, verifique e tente novamente.")).when(livroService).deleteById(Mockito.anyLong());
+        ResponseEntity<Object> retorno = livroController.delete(1L);
+        assertEquals(HttpStatus.NOT_FOUND,retorno.getStatusCode());
+    }
+
+    @Test
+    void aoDeletar() {
+        Mockito.doNothing().when(livroService).deleteById(Mockito.anyLong());
+        ResponseEntity<Object> retorno = livroController.delete(1L);
+        assertEquals(HttpStatus.OK,retorno.getStatusCode());
+    }
+
+
+}
---
+++ src/test/java/com/bookmanagement/api/service/LivroServiceTest.java
@@ -0,0 +1,97 @@
+package com.bookmanagement.api.service;
+
+import com.bookmanagement.api.dto.LivroRecordDto;
+import com.bookmanagement.api.model.LivroModel;
+import com.bookmanagement.api.repository.LivroRepository;
+import org.hibernate.ObjectNotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Collections;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+@SpringBootTest
+class LivroServiceTest {
+
+    @InjectMocks
+    private LivroService livroService;
+
+    @Mock
+    private LivroRepository livroRepository;
+
+    LivroModel livroTest = new LivroModel(
+            1L,
+            "Harry Potter e a Pedra Filosofal",
+            "J.K. Rowling",
+            1997
+    );
+    LivroRecordDto livroRecordDtoTest = new LivroRecordDto(
+            "Harry Potter e a Pedra Filosofal",
+            "J.K. Rowling",
+            1997
+    );
+
+    @Test
+    void aoSalvarRetornoUmObjetoLivro() {
+        Mockito.when(livroRepository.save(Mockito.any())).thenReturn(livroTest);
+        var retorno = livroService.save(Mockito.any(LivroModel.class));
+        assertNotNull(retorno);
+        assertEquals(livroTest, retorno);
+    }
+
+    @Test
+    void aoBuscarTodosRetornarUmaLista() {
+
+        Mockito.when(livroRepository.findAll()).thenReturn(Collections.singletonList(livroTest));
+        var retorno = livroService.findAll();
+        assertNotNull(retorno);
+        assertEquals(Collections.singletonList(livroTest) , retorno);
+        assertEquals(1,retorno.size());
+    }
+
+    @Test
+    void aoBuscarPorIdRetornarUmObjetoLivro() {
+        Mockito.when(livroRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(livroTest));
+        var retorno = livroService.findById(1L);
+        assertNotNull(retorno);
+        assertEquals(Optional.of(livroTest), retorno);
+    }
+
+    @Test
+    void aoAtualizardeveLancarExcecaoQuandoLivroNaoExistir() {
+        Mockito.when(livroRepository.findById(Mockito.anyLong())).thenReturn(Optional.empty());
+        Assertions.assertThrows(ObjectNotFoundException.class, () -> {
+            livroService.update(1L, livroRecordDtoTest);
+        });
+    }
+    @Test
+    void AoAtualizarRetornarUmObjetoLivro() {
+        Mockito.when(livroRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(livroTest));
+        Mockito.when(livroService.save(Mockito.any(LivroModel.class))).thenReturn(livroTest);
+        var retorno = livroService.update(1L, livroRecordDtoTest);
+        assertEquals(livroTest, retorno);
+    }
+
+    @Test
+    void aoDeletardeveLancarExcecaoQuandoLivroNaoExistir() {
+        Mockito.when(livroRepository.findById(Mockito.anyLong())).thenReturn(Optional.empty());
+        Assertions.assertThrows(ObjectNotFoundException.class, () -> {
+            livroService.deleteById(Mockito.anyLong());
+        });
+    }
+
+    @Test
+    void aoDeletarDeveExecutarComSucessoQuandoLivroExistir() {
+        Mockito.when(livroRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(livroTest));
+        Mockito.doNothing().when(livroRepository).deleteById(Mockito.anyLong());
+        livroService.deleteById(1L);
+        Mockito.verify(livroRepository, Mockito.times(1)).deleteById(Mockito.anyLong());
+
+    }
+}
